d568b06074752dac316db4cac1912517ce58c81a
André Mendes <andre.mendesc@gmail.com>
2020-04-19 22:40:26 -0300

    Ajusta colisão do bozo deitado e livros baixos que não encostam no chão #2

--- source/BozoRunGameState.hx
+++ source/BozoRunGameState.hx
@@ -162,7 +162,7 @@ class BozoRunGameState extends FlxState
 		_bozo.scale.set(1, 1);
 		
 		_bozoDeitado = new FlxSprite().loadGraphic(AssetPaths.jairtomando__png, true, 122, 140);
-		_bozoDeitado.height = 50;
+		_bozoDeitado.height = 40;
 		_bozoDeitado.offset.set(0, 90);
 		_bozoDeitado.visible = false;
 		// set animations to use this run
@@ -331,7 +331,7 @@ class BozoRunGameState extends FlxState
 	}
 
 	private inline function processaColisaoBozoLivros(_obj1, _obj2):Void { 
-		if ((_obj1.x + 30) < _obj2.x && (_obj1.y - 50) < _obj2.y && _amountOranges >= 1) {
+		if ((_obj1.x + 30) < _obj2.x && (_obj1.y - 60) < _obj2.y && _amountOranges >= 1) {
 			tocarPluftAnimacao(_obj2.x, _obj2.y);
 			_obj2.destroy();
 			if(_amountOranges >= 1) _amountOranges--;
